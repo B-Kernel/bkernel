32c80717c1310a0515dde524394b6e6f3c060738
Erik <122167559+EriktheRDev@users.noreply.github.com>
2023-01-17 23:47:36 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+import sys
+import shutil
 
 os.system('color')
 
 # Pre-determined variables
 vardir = [
-  ["Bootloader.fun","Registry.fun"],
+  ["Bootloader.fn","Registry.fn"],
   ["Booted.var","Location.var","Locationdir.var","cman.var"],
   ["Command.inp","Echo.inp"],
   ["Vardir.lst"],
@@ -50,11 +52,17 @@ def print_error(error_code, print_error=True):
 
 def help_command(command=None):
   if (command == None or command == ""):
+    os.system('cls')
     return "\nTo get help with a specific command, type help <command>\n"
   elif (command == "help"):
+    os.system('cls')
     return "\nbruh\n"
   elif (command == "rd"):
-    return "\nReads a directory you specify.\n\n\tExample: rd about\n\nSyntax:\n\trd <directory_name>\n\nReturns:\n\tThe directory contents of the directory name you specified. In case you specify an invalid/non-existent directory, you will get an error code of 0x004.\n"
+    os.system('cls')
+    return "\nReads a directory you specify.\n\n\tExample: rd About\n\nSyntax:\n\trd <directory_name>\n\nReturns:\n\tThe directory contents of the directory name you specified. In case you specify an invalid/non-existent directory, you will get an error code of 0x004.\n"
+  elif (command == "rf"):
+    os.system('cls')
+    return "\nReads a directory you specify.\n\n\tExample: rf /workspaces/bkernel/docs/Test\n\nSyntax:\n\trf <location of file (path)>\n\nReturns:\n\tThe contents of the filename you specified. In case you specify an invalid/non-existent file, you will get an error code of 0x002.\n"
 
 #Imported Extensions
 import random
@@ -69,17 +77,17 @@ def Bootloader():
     print(f"{bcolors.WARNING}Welcome to {bcolors.BOLD}B Kernel{bcolors.ENDC}")
     return True
   else:
-    print_error("0x001");
+    print_error("0x001")
 #Registry
 def Registry(x = 0):
   if x == 0: #Prints all Commands
-    print(vardir[0]) #.fun Commands
+    print(vardir[0]) #.fn Commands
     print(vardir[1]) #.var Commands
     print(vardir[2]) #.inp Commands
     print(vardir[3]) #.lst Commands
     print(vardir[4]) #.bool Commands
-  elif x == "fun":
-    print(vardir[0]) #only prints .fun Commands
+  elif x == "fn":
+    print(vardir[0]) #only prints .fn Commands
   elif x == "var":
     print(vardir[1]) #only prints .var Commands
   elif x == "inp":
@@ -96,8 +104,8 @@ booted = Bootloader()
 while booted == True:
   command = str(input(locationdir))
   if "registry" in command:
-    if command == "registry fun":
-      Registry("fun")
+    if command == "registry fn":
+      Registry("fn")
     elif command == "registry var":
       Registry("var")
     elif command == "registry inp":
@@ -109,12 +117,12 @@ while booted == True:
     elif command == "registry":
       Registry()
     else:
-      print_error("0x002");
+      print_error("0x002")
   elif "execute" in command:
-    if command == "execute Bootloader.fun":
+    if command == "execute Bootloader.fn":
       Bootloader()
       print("Completed.")
-    elif command == "execute Registry.fun":
+    elif command == "execute Registry.fn":
       Registry()
       print("Completed")
     elif command == "execute Booted.bool":
@@ -138,13 +146,13 @@ while booted == True:
         locationdir = "/workspaces/bkernel/about "
         print("Done!")
       else:
-        print_error("0x004");
+        print_error("0x004")
     elif command == "cd 0":
       if location == 1:
         location = 0
         locationdir = "/workspaces/bkernel "
       else:
-        print_error("0x004");
+        print_error("0x004")
   elif command == "help":
     command_help = input("What command do you need help with? ")
     print(help_command(command_help))
@@ -153,14 +161,33 @@ while booted == True:
       print(os.listdir(os.path.dirname(os.path.realpath(__file__))))
     elif location == 1:
       print(os.listdir(os.path.dirname(os.path.realpath(__file__)) + r"/About"))
-  elif "rdi" in command:
+  elif "rf" in command:
     echo = input("Insert Path: ")
     cman = open(echo,"r")
     print(cman.read())
     cman.close
+  elif "wf" in command:
+    echo = input("Insert Name: ")
+    with open("docs/" + echo, 'w') as f:
+      os.system('cls')
+      print("Editing File with Name (" + echo + ").")
+      f.write(input(""))
+  elif "df" in command:
+    echo = input("Insert Path: ")
+    os.remove(echo)
+  elif "ded" in command:
+    echo = input("Insert Directory Path: ")
+    os.rmdir(echo)
+  elif "dd" in command:
+    echo = input("Insert Directory Path: ")
+    try:
+      shutil.rmtree(echo)
+    except OSError as Error:
+      print_error("0x004")
   elif "cls" in command:
     os.system('cls')
   elif "exit" in command:
+    os.system('cls')
     print("Shutting Down...")
     time.sleep(random.randint(0, 5))
     booted = False
