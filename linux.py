05f829af15fdc9393e977955e2a6bdd530fc6339
Erik <122167559+EriktheRDev@users.noreply.github.com>
2023-02-09 13:58:31 -0500

    Bidirectional RGB and HEX conversion

--- linux.py
+++ linux.py
@@ -353,7 +353,7 @@ while booted == True:
         octalpos += 1
         octalresult += str(i)
     print("OCTAL: " + octalresult)
-    hexa = hex(echo)
+    hexa = hex(int(echo))
     hexalist = []
     hexaresult = ""
     for i in hexa:
@@ -367,6 +367,102 @@ while booted == True:
         hexapos += 1
         hexaresult += str(i)
     print("HEXADECIMAL: " + hexaresult)
+  elif command == "hex_rgb convert":
+    hex = input("Insert HEX: ")
+    hexdictionary = {
+      "0":"0",
+      "1":"1",
+      "2":"2",
+      "3":"3",
+      "4":"4",
+      "5":"5",
+      "6":"6",
+      "7":"7",
+      "8":"8",
+      "9":"9",
+      "a":"10",
+      "b":"11",
+      "c":"12",
+      "d":"13",
+      "e":"14",
+      "f":"15",
+      "A":"10",
+      "B":"11",
+      "C":"12",
+      "D":"13",
+      "E":"14",
+      "F":"15"
+    }
+    hex1 = []
+    hex2 = []
+    hex3 = []
+    hex1.append(hex[0])
+    hex1.append(hex[1])
+    hex2.append(hex[2])
+    hex2.append(hex[3])
+    hex3.append(hex[4])
+    hex3.append(hex[5])
+    R = []
+    G = []
+    B = []
+    for i in hex1:
+      i = int(hexdictionary[i])
+      R.append(i)
+    for i in hex2:
+      i = int(hexdictionary[i])
+      G.append(i)
+    for i in hex3:
+      i = int(hexdictionary[i])
+      B.append(i)
+    r = R[0] * 16 + R[1]
+    g = G[0] * 16 + G[1]
+    b = B[0] * 16 + B[1]
+    print(str(r) + ", " + str(g) + ", " + str(b))
+      
+  elif command == "rgb_hex convert":
+    #inputs
+    rgbdictionary = {
+      "0":"0",
+      "1":"1",
+      "2":"2",
+      "3":"3",
+      "4":"4",
+      "5":"5",
+      "6":"6",
+      "7":"7",
+      "8":"8",
+      "9":"9",
+      "10":"A",
+      "11":"B",
+      "12":"C",
+      "13":"D",
+      "14":"E",
+      "15":"F"
+    }
+    R = int(input("Insert R: "))
+    G = int(input("Insert G: "))
+    B = int(input("Insert B: "))
+    def rgbtohex(r,g,b,hexdict):
+      rtime = 0
+      gtime = 0
+      btime = 0
+      while r >= 16:
+        rtime += 1
+        r -= 16
+      while g >= 16:
+        gtime += 1
+        g -= 16
+      while b >= 16:
+        btime += 1
+        b -= 16
+      r = hexdict[str(r)]
+      rtime = hexdict[str(rtime)]
+      g = hexdict[str(g)]
+      gtime = hexdict[str(gtime)]
+      b = hexdict[str(b)]
+      btime = hexdict[str(btime)]
+      print(str(rtime) + str(r) + str(gtime) + str(g) + str(btime) + str(b))
+    rgbtohex(R,G,B,rgbdictionary)
   elif command == "calc":
     #B-Kernel Calculator
     import os
