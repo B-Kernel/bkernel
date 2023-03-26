9f8e8c4de757fcd301ba3eca9446a803fe4516ad
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-03-26 15:33:19 -0400

    added a win tip

--- bin/functions/wintips.py
+++ bin/functions/wintips.py
@@ -1,5 +1,5 @@
 import random
 import os
 os.system('cls')
-facts = ["Use 'cf' to copy files.", "You can use 'rc' to compile python", "B-Kernel has a discord server!", "'ded' Deletes empty directories, while 'dd' Deletes directories and their contents.", "B-Kernel Web is the online version of B-Kernel!"]
+facts = ["Use 'cf' to copy files.", "You can use 'rc' to compile python", "Pressing Shift + Click on the Taskbar [when clicking on a window] opens up another window.","B-Kernel has a discord server!", "'ded' Deletes empty directories, while 'dd' Deletes directories and their contents.", "B-Kernel Web is the online version of B-Kernel!"]
 print("[i] - " + facts[random.randint(0, 5)])
